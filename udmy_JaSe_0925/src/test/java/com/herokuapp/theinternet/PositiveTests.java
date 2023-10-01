2b351b653368804dc94ec60ae843c02dfdb030d2
salorod <salorod@localhost>
2023-10-01 10:41:28 -0500

    1.-Adding new testng class 'negativeTest' 2.-Updated dependencies

--- udmy_JaSe_0925/src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ udmy_JaSe_0925/src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -17,10 +17,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 	  public void firstTest() {
 		 
 		 System.out.println("--->Test Started");
-		  
+		  /*
 		  ChromeOptions chromeOpt = new ChromeOptions();
 		  WebDriverManager.chromedriver().setup();
 		  WebDriver driver = new ChromeDriver(chromeOpt);
+		  */
+		  WebDriver driver = new ChromeDriver();
 
 		  //driver.get("https://www.google.com/");
 		  //driver.manage().window().maximize();
@@ -31,8 +33,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
         	//2.-Maximize page
         	driver.manage().window().maximize();
         	
+        	sleep(3000);
     		// IMPLICIT WAIT
-    		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    		//driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         	System.out.println("--->Page is opened");
         	
     		//3.-Login
@@ -80,62 +83,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
                	
 	  }
-	 
-	 public void incorrectUserName() {
-		 System.out.println("--->Test Started");
-		  
-		  ChromeOptions chromeOpt = new ChromeOptions();
-		  WebDriverManager.chromedriver().setup();
-		  WebDriver driver = new ChromeDriver(chromeOpt);
-
-		  //driver.get("https://www.google.com/");
-		  //driver.manage().window().maximize();
-       	//Assert.assertEquals(driver.getCurrentUrl(), "https://www.google.com/");
-       	
-       	//1.-Open browser and enter the url
-       	driver.get("https://the-internet.herokuapp.com/login");
-       	//2.-Maximize page
-       	driver.manage().window().maximize();
-       	
-   		// IMPLICIT WAIT
-   		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-       	System.out.println("--->Page is opened");
-       	
-   		//3.-Login
-       	//username: //input[@name='username']
-       	//tomsmith
-       	WebElement username = driver.findElement(By.name("username"));
-       	username.sendKeys("213smith");
-       	
-       	//pass:	//input[@name='password']
-       	//SuperSecretPassword!
-       	WebElement password = driver.findElement(By.name("password"));
-       	password.sendKeys("SuperSecretPassword!");
-       	
-       	//login button is visible: 	//*[contains(@class,'sign-in')]  ||		//*[contains(text(),' Login')]
-       	WebElement loginButton = driver.findElement(By.xpath("//*[contains(@class,'sign-in')]")); 
-       	loginButton.click();
-       	
-       	sleep(3000);
-       	
-      //succes URL
-		String expectedUrl = "https://the-internet.herokuapp.com/secure";
-		String actualUrl = driver.getCurrentUrl();
-		Assert.assertEquals(actualUrl, expectedUrl, "Actual page url is not the same as expected");
-    	
-   
-    	//    succesful login message
-    	WebElement succesMessage = driver.findElement(By.cssSelector("#flash"));
-    	String expectedMessage = "You logged into a secure area!";
-    	String actualMessage = succesMessage.getText();
-    	System.out.println("actual Message:" + actualMessage);
-    	Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain expected message.\nActual Message: " + actualMessage + "\nExpected MEssage: " + expectedMessage);
-    	
-    	driver.close();
-    	driver.quit();
-    	System.out.println("--->Test Finished");
-
-	 }
+	
 	 
 	 // HARD WAIT JAVA
  	private void sleep(long m) {
